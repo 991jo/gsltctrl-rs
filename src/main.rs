44c2bb1c5113a8e56d8c5e62b8d43d199afd748b
Jochen Kupperschmidt <homework@nwsnet.de>
2023-07-08 12:37:10 +0200

    Extract CLI parsing into separate module

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,9 @@
-use clap::Parser;
 use reqwest::blocking::Client;
 use reqwest::Method;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::env;
-
-#[derive(Parser, Debug)]
-#[command(
-    author,
-    version,
-    long_about = "A tool to generate and renew Steam Game Server License Tokens (GSLT).
-
-This tool will print out a valid token for the given appid and memo.
-If a token already exists it returns that token.
-If it was expired it gets renewed beforehand.
-If no token for that appid and memo existed a new one is created.
-
-appid and memo are given as cli parameters.
-The web API key is read from the environment variable GSLTCTRL_TOKEN.
-This is done to prevent leaking the API key via the process listing."
-)]
-struct Args {
-    /// the appid for which to create a token
-    appid: u32,
-
-    /// the memo string. Hast to be unique per appid
-    memo: String,
-}
+mod cli;
 
 /// A Struct representing the API
 #[derive(Debug)]
@@ -193,7 +170,7 @@ impl GameServersService {
 }
 
 fn main() {
-    let args = Args::parse();
+    let args = cli::parse_args();
     let apitoken = get_apitoken();
 
     let service = GameServersService::new(&apitoken);
